8fa55884bf1199da833998c8f688ccf44c548829
Diana <liahhh_1@yahoo.com>
2019-11-15 03:02:47 -0300

    Creating a debug for fight and LOTS of refactor

--- game/Model/Npc-Human-Model.js
+++ game/Model/Npc-Human-Model.js
@@ -1,18 +1,18 @@
 /* Creating a model for human npc */
 /* Defining functions */
 
-window.NpcModel = function(age, gender, pronoun, maxHealth,
+window.NpcModel = function(adult, gender, pronoun, maxHealth,
                             lefthand, righthand, penis, vagina,
                             mouth, chest, description)
 {
-    this.age = age || 0;
+    this.adult = adult || 0;
     this.gender = gender || 0;
     this.pronoun = pronoun || 0;
     this.maxHealth = maxHealth || 200;
     this.health = maxHealth || 200;
     this.lefthand = lefthand || "none";
     this.righthand = righthand || "none";
-    this.genitals = penis || "none";
+    this.penis = penis || "none";
     this.vagina = vagina || "none";
     this.mouth = mouth || "none";
     this.chest = chest || "none";
