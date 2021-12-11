336693ec4c9e2364b9eb881fb020a22f97e57b86
myonmyuu <myonmyuu@gmail.com>
2021-12-11 01:24:22 +0100

    extend and improve 3d
    - renamed file with 3d functions for clarity
    - added gltf loader to load custom models
    - actually hid the time/location display
    - replaced characteristics/character creation sprite with 3d renderer
    - started work on character 3d model, is currently loaded from my web server

---
+++ game/03-JavaScript/DOL3D.js
@@ -0,0 +1,221 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.DOL3D = {}));
+}(this, (function (exports) { 'use strict';
+
+		var Loader = null;
+		function GetLoader() {
+			if (Loader == null){
+				Loader = new THREE.GLTFLoader();
+			}
+			return Loader;
+		}
+		
+
+		DOL3D.DataAddress = "http://37.59.47.180/data/"
+
+		DOL3D.CharSizes = {
+			"Tiny": 0.76,
+			"Small": 0.825,
+			"Normal": 0.925,
+			"Large": 1.025
+		};
+
+		DOL3D.DisplayType = {
+			Combat: {
+				Width: 850,
+				Height: 400
+			},
+			Canvas:	{
+				Width: 280,
+				Height: 228,
+			},
+			CreateChar: {
+				Width: 228,
+				Height: 200
+			},
+			Characteristics: {
+				Width: 200,
+				Height: 200
+			}
+		};
+
+		// just for testing and learning how data in dol is stored in combat
+		function* _ActiveNPCIterator() {
+			for(let i = 0; i < V.enemyno; i++) {
+				yield V.NPCList[i];
+			}
+		};
+		DOL3D.GetActiveNPCs = function () {
+			return Array.from(_ActiveNPCIterator());
+		};
+		function DescribeNPCs() {
+			let npcs = DOL3D.GetActiveNPCs();
+			let output = "";
+			for (let i = 0; i < npcs.length; i++) {
+				let npc = npcs[i];
+				output += npc.fullDescription + (i != npcs.length - 1 ? "," : "") + " <br>";
+			}
+			return output;
+		}
+		DOL3D.DescribeNPCs = DescribeNPCs;
+
+		function CreateRenderer(dispType) {
+			let renderer = new THREE.WebGLRenderer( { antialias: true } );
+			let height = 100, width = 100;
+
+			if (dispType != undefined) {
+				height = dispType.Height;
+				width = dispType.Width;
+			}
+
+			renderer.setSize( width, height );
+			return renderer;
+		};
+		function CreateCamera(dispType) {
+			if (dispType == undefined)
+				return new THREE.PerspectiveCamera( 70, 1, 0.001, 2000 );
+
+			return new THREE.PerspectiveCamera( 70, dispType.Width / dispType.Height, 0.001, 2000 );
+		};
+
+
+		function CreateCube() {
+			let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			let material = new THREE.MeshNormalMaterial();
+			return new THREE.Mesh( geometry, material );
+		}
+
+		async function GetMesh(url) {
+			let loader = GetLoader();
+			let data = await loader.loadAsync(DOL3D.DataAddress + url);
+			return data.scene.children[0];
+		}
+
+		async function CreateChar(sizeName) {
+
+			if (sizeName == undefined)
+				sizeName = "Normal" 
+			let size = DOL3D.CharSizes[sizeName];
+
+			let mesh = await GetMesh("char.glb");
+			mesh.scale.copy(new THREE.Vector3(size, size, size));
+			mesh.material = new THREE.MeshNormalMaterial();
+			return mesh;
+		}
+
+		function TestAnimation( time, mesh ) {
+			mesh.rotation.x = time / 2000;
+			mesh.rotation.y = time / 1000;
+		}
+
+		async function ShowCombat(output) {
+			let type = DOL3D.DisplayType.Combat;
+
+			let renderer = CreateRenderer(type);
+			output.append( renderer.domElement );
+
+			let camera = CreateCamera(type);			
+			camera.position.z = 2.5;
+
+			let scene = new THREE.Scene();
+	
+			let mesh = await CreateChar();
+			scene.add( mesh );
+
+			let small = await CreateChar("Small");
+			small.position.set(-1, 0, 0);
+			scene.add(small);
+			let tiny  = await CreateChar("Tiny");
+			tiny.position.set(-2, 0, 0);
+			scene.add(tiny);
+			let large = await CreateChar("Large");
+			large.position.set(1, 0, 0);
+			scene.add(large);
+
+			let cube = CreateCube();
+			cube.position.set(2, 1.5, 0)
+			scene.add(cube);
+			//console.log(mesh);
+
+			renderer.setAnimationLoop((time) => {
+				//TestAnimation(time, mesh);
+				renderer.render( scene, camera );
+			});
+			
+		};
+		DOL3D.ShowCombat = function (output) {
+			ShowCombat(output);
+		};
+
+		async function ShowCanvas(output) {
+			let type = DOL3D.DisplayType.Canvas;
+
+			let renderer = CreateRenderer(type);
+			output.append( renderer.domElement );
+
+			let camera = CreateCamera(type);
+			camera.position.z = 2;
+
+			let scene = new THREE.Scene();
+
+			let mesh = await CreateChar();
+			scene.add(mesh);
+			scene.add(CreateCube());
+			//console.log(mesh);
+			
+			renderer.setAnimationLoop((time) => {
+				TestAnimation(time, mesh);
+				renderer.render( scene, camera );
+			});
+		};
+		DOL3D.ShowCanvas = function(output) {
+			ShowCanvas(output);
+		};
+
+
+		async function ShowCreate(output) {
+			let type = DOL3D.DisplayType.CreateChar;
+			let char;
+			let render = CreateRenderer(type);
+			output.append( render.domElement );
+
+			let scene = new THREE.Scene();
+			let cam = CreateCamera(type);
+			cam.position.z = 2;
+			scene.add(cam);
+			scene.add(CreateCube());
+
+
+			render.setAnimationLoop((time) => {
+
+
+
+				render.render(scene, cam);
+			})
+		};
+		DOL3D.ShowCreate = ShowCreate;
+
+		async function ShowChara(output) {
+			let type = DOL3D.DisplayType.Characteristics;
+			let char;
+			let render = CreateRenderer(type);
+			output.append( render.domElement );
+
+			let scene = new THREE.Scene();
+			let cam = CreateCamera(type);
+			cam.position.z = 2;
+			scene.add(cam);
+			scene.add(CreateCube());
+
+
+			render.setAnimationLoop((time) => {
+
+
+
+				render.render(scene, cam);
+			})
+		};
+		DOL3D.ShowChara = ShowChara;
+})));
